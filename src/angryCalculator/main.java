1b7059349439f406c5cc7d19bb57d08b34fa4172
dapastushenko <dapastushenko@yandex.ru>
2017-11-12 11:52:39 +0300

    angry Accumulator start

---
+++ src/angryCalculator/main.java
@@ -0,0 +1,19 @@
+package angryCalculator;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+        String x;
+        String y;
+        Scanner str = new Scanner(System.in);
+        System.out.println("Введите значение переменной х");
+        x=str.nextLine();
+        parse(x);
+
+    }
+
+    private static void parse(String x) {
+        f
+    }
+}
