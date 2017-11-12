92bc888397e7cd547ef39fe6cdfdb1d3cec5006e
dapastushenko <dapastushenko@yandex.ru>
2017-11-12 23:08:19 +0300

    доработать

--- src/angryCalculator/Main.java
+++ src/angryCalculator/Main.java
@@ -1,21 +1,27 @@
 package angryCalculator;
 
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         String x;
         String y;
+
         Scanner str = new Scanner(System.in);
         System.out.println("Введите значение переменной х");
         x=str.nextLine();
-        parse(x);
+//        parse(x);
+        y="x = 1";
+        String[] split = y.split(" ");
+        for ( String o: split){
+            System.out.println(o);
+        }
+
 
     }
+//    public static String[] parse(String x) {
 
-    private static String[] parse(String x) {
+//        return;
+//    }
 
-        return new String[0];
-    }
 }
