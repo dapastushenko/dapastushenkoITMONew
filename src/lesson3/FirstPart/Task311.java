fe77072ff0c4809ac1f390685d65fc4859ccc1a3
dapastushenko <dapastushenko@yandex.ru>
2017-11-22 20:02:13 +0300

    refactor package

---
+++ src/lesson3/FirstPart/Task311.java
@@ -0,0 +1,43 @@
+/*
+Пользователь должен указать с клавиатуры чётное положительное число, а программа должна создать массив указанного
+размера из случайных целых чисел из [-5;5] и вывести его на экран в строку. После этого программа должна определить и
+сообщить пользователю о том, сумма модулей какой половины массива больше: левой или правой, либо сообщить, что эти
+суммы модулей равны. Если пользователь введёт неподходящее число, то программа должна требовать повторного ввода до
+тех пор, пока не будет указано корректное значение.
+ */
+package lesson3.FirstPart;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task311 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите целое число:");
+        int arrayEnd = in.nextInt();
+        int abssummleft = 0, abssummright = 0;
+        int[] rndarray = new int[arrayEnd];
+
+        if (arrayEnd % 2 == 0 && arrayEnd!=0) {
+            for (int i = 0; i <= arrayEnd - 1; i++) {
+                rndarray[i] = (int) (Math.random() * (6 - (-5)) + (-5));
+            }
+            for (int i = 0; i <= (arrayEnd / 2) - 1; i++) {
+                abssummleft = abssummleft + Math.abs(rndarray[i]);
+            }
+            for (int i = arrayEnd - 1; i >= (arrayEnd / 2); i--) {
+                abssummright = abssummright + Math.abs(rndarray[i]);
+            }
+            System.out.println(Arrays.toString(rndarray));
+            if (abssummleft > abssummright) {
+                System.out.println("Сумма больше в левой половине");
+            }
+            else {
+                System.out.println("Сумма больше в правой половине");
+            }
+        } else {
+            System.out.println("Ошибка, вы ввели нечетное число либо 0");
+        }
+
+    }
+}
---
+++ src/lesson3/FirstPart/Task38.java
@@ -0,0 +1,29 @@
+/*
+Создайте массив из 12 случайных целых чисел из отрезка [-15;15]. Определите какой элемент является в этом массиве
+максимальным и сообщите индекс его последнего вхождения в массив.
+ */
+
+package lesson3.FirstPart;
+
+import java.util.Arrays;
+
+public class Task38 {
+    public static void main(String[] args) {
+        int[] rnd = new int[30];
+        int position=0, max = 0;
+
+        for (int i = 0; i <= 29; i++) {
+            rnd[i] = (int) (Math.random() * (16 - (-15)) + (-15));
+
+            if (i == 0) {
+                max = rnd[i];
+                position = i;
+            } else if (i > 0 && max<rnd[i]) {
+                max = rnd[i];
+                position = i;
+            }
+        }
+        System.out.println(Arrays.toString(rnd));
+        System.out.println("Максимально число:" + max + " Позиция:" + position);
+    }
+}
